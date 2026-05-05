dda8f062014c974d6cdf7ba3200f20e9df76947e
Салосин Георгий Сергеевич <SalosinGS@dom201.local>
2026-05-05 09:36:37 +0700

    initial lab06

--- lab04/05.js
+++ lab04/05.js
@@ -12,3 +12,31 @@ if (number !== null)
 {
     alert(`Вы ввели ${number}`);
 }
+
+
+
+alert("task 6.");
+
+function showPrimes(n) 
+{
+    for (let i = 2; i <= n; i++) 
+    {
+        let isPrime = true;
+        
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime) 
+        {
+            alert(i);
+        }
+    }
+}
+
+a = Number(30);
+
+showPrimes(a); 
--- lab04/06.js
+++ lab04/06.js
@@ -1,26 +1,2 @@
-alert("task 6.");
 
-function showPrimes(n) 
-{
-    for (let i = 2; i <= n; i++) 
-    {
-        let isPrime = true;
-        
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        
-        if (isPrime) 
-        {
-            alert(i);
-        }
-    }
-}
-
-let a = Number(100);
-
-showPrimes(a); 
 
