86e2d61d2105d6ea78235f751379b33bdfc30012
SalosinGS <gsalosin@gmail.com>
2026-05-19 10:13:48 +0700

    finish task 1

--- lab08/lab08.js
+++ lab08/lab08.js
@@ -1,17 +1,79 @@
 // TASK 1
 
 //1
-function counter(n) {
-  let current = n;
-  
-  const intervalId = setInterval(() => {
-    console.log(current);
+let simpleInterval = null;
+
+function runCounter() {
+    const n = parseInt(document.getElementById('counterN').value);
+    const outputDiv = document.getElementById('counterOutputContent1');
     
-    if (current === 0) {
-      clearInterval(intervalId);
-    }
+    outputDiv.innerHTML = '';
+    outputDiv.innerHTML += `Запуск:<br>`;
     
-    current--;
-  }, 1000);
+    if (simpleInterval) clearInterval(simpleInterval);
+    
+    let current = n;
+    simpleInterval = setInterval(() => {
+        outputDiv.innerHTML += `${current}<br>`;
+        
+        if (current === 0) {
+            clearInterval(simpleInterval);
+            outputDiv.innerHTML += ` Готово!<br>`;
+        }
+        current--;
+    }, 1000);
 }
+
+
+
+//2
+let timer = null;
+let num = 0;
+let maxNum = 0;
+
+function createAndStart() {
+    if (timer) clearInterval(timer);
+    
+    maxNum = parseInt(document.getElementById('createCounterN').value);
+    num = maxNum;
+    
+    document.getElementById('createCounterOutputContent').innerHTML = `Счёт от ${maxNum} до 0:<br>`;
+    
+    timer = setInterval(() => {
+        if (num >= 0) {
+            document.getElementById('createCounterOutputContent').innerHTML += `${num}<br>`;
+            if (num === 0) clearInterval(timer);
+            num--;
+        }
+    }, 1000);
+}
+
+function pauseCounter() {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    }
+}
+
+function resumeCounter() {
+    if (!timer && num >= 0) {
+        timer = setInterval(() => {
+            if (num >= 0) {
+                document.getElementById('createCounterOutputContent').innerHTML += `${num}<br>`;
+                if (num === 0) clearInterval(timer);
+                num--;
+            }
+        }, 1000);
+    }
+}
+
+function stopCounter() {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    }
+    num = maxNum;
+    document.getElementById('createCounterOutputContent').innerHTML = `Сброшено до ${maxNum}<br>`;
+}
+
 
